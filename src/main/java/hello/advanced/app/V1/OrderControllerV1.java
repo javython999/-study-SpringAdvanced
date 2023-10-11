262fdbe65e5fde21b134b0a101a471775a1d3f14
javython999 <infinity2544@gmail.com>
2023-10-12 08:45:58 +0900

    로그 추적기 V2

---
+++ src/main/java/hello/advanced/app/V1/OrderControllerV1.java
@@ -0,0 +1,36 @@
+package hello.advanced.app.V1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    TraceStatus status = null;
+    @GetMapping("/v1/request")
+    public String request(String itemId) {
+
+        status = trace.begin("OrderControllerV1.request()");
+        try {
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/hello/advanced/app/V2/OrderControllerV2.java
@@ -0,0 +1,36 @@
+package hello.advanced.app.V2;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    TraceStatus status = null;
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+
+        status = trace.begin("OrderControllerV1.request()");
+        try {
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+
+    }
+
+
+
+
+
+}
